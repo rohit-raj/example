05e653a249817965bd481e3f28c4488cb4ba95b4
Rohit Raj <rohit@campusdiaries.com>
2017-07-11 01:42:00 +0530

    Master: sender and test commit

---
+++ sender.js
@@ -0,0 +1,4 @@
+curl -i -s -k  -X $'POST' \
+    -H $'Origin: https://csp-report.com' -H $'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36' -H $'Content-Type: application/json' -H $'Referer: https://csp-report.com/' \
+    -d @test.json \
+    $'https://csp-report.com/app2/csp/_report?api_key=5d084f5b1b5144ddb260a27b2bb25b'
