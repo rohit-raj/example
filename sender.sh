fe523e7efa3351a71b91673f0e2bae8b00873f68
Rohit Raj <rohitrjsinha@gmail.com>
2017-07-11 02:58:52 +0530

    Master: f commit

--- sender.sh
+++ sender.sh
@@ -1,7 +1,2 @@
-# curl -i -s -k  -X $'POST' \
-#     -H $'Origin: https://csp-report.com' -H $'Content-Type: application/json' -H $'Referer: https://csp-report.com/' \
-#     -d @/var/lib/jenkins/jobs/example/workspace/test.json \
-#     $'https://csp-report.com/app2/csp/_report?api_key=5d084f5b1b5144ddb260a27b2bb25b'
-
-
-curl -X POST -H "Content-Type: application/json" -d @/var/lib/jenkins/jobs/example/workspace/test.json https://csp-report.com/app2/csp/_report?api_key=5d084f5b1b5144ddb260a27b2bb25b
+curl -X POST -H "Content-Type: application/json" -d @/var/lib/jenkins/jobs/example/workspace/test.json \
+    https://csp-report.com/app2/csp/_report?api_key=5d084f5b1b5144ddb260a27b2bb25b
